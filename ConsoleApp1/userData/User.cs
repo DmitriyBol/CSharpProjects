8d55a166e56f0fe552a5b4a4c4b697c3125e73d6
Dmitriy B <72795383+DmitriyBol@users.noreply.github.com>
2023-02-17 12:46:40 +0500

    added datastamps, deleg. classes and types

---
+++ ConsoleApp1/userData/User.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace ConsoleApp1.userData;
+
+public class User
+{
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime LastUpdatedDate { get; set; }
+
+    public User(string name, string lastName, int age)
+    {
+        DateTime createStamp = DateTime.Now;
+
+        Name = name;
+        LastName = lastName;
+        Age = age;
+        CreatedDate = createStamp;
+    }
+
+    public void updateChangeDate(DateTime date)
+    {
+        LastUpdatedDate = date;
+    }
+}
---
+++ ConsoleApp1/userData/WriteData.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ConsoleApp1.userData;
+
+public class WriteData
+{
+    private static readonly JsonSerializerOptions Options = 
+        new() { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull };
+    
+    public static void WriteDataInFile(string name, string lastName, int age)
+    {
+        if (name != "" && lastName != "" && age != 0)
+        {
+            User newUser = new User(name, lastName, age);
+            DateTime timeStamp = DateTime.Now;
+            newUser.updateChangeDate(timeStamp);
+
+            var jsonData = JsonSerializer.Serialize(newUser, Options);
+            
+            Console.WriteLine("User successfully created!");
+            Console.WriteLine("Now restart the app to work with it.");
+            
+            File.WriteAllLinesAsync("../../testData.txt", new[] {jsonData});
+        }
+    }
+}
